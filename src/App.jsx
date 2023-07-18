1e02ed5ac3d2556ae9c7a9248ec48fde9b8522e1
Joyonto Karmakar <joyonto1997@gmail.com>
2023-07-18 11:36:58 +0600

    project setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {ROOT_PATH} from "./routes/Slug.jsx";
 import Header from "./components/Header.jsx";
+import Home from "./pages/home.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
 
   return (
     <>
-      <div className={''}>
+      <BrowserRouter>
+        <div className={'font-roboto'}>
           <Header/>
-
-      </div>
+          <Routes>
+            <Route path={ROOT_PATH} element={<Home/>} />
+          </Routes>
+          <Footer/>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#58BF73',
+        greenShed1: '#1F8E3D',
+        greenShed2: '#B4ECD2',
+        greenShed3: '#93D1B5',
+        greenShed4: '#64B992',
+        black1: '#181F1B',
+        black2: '#1A1A1A',
+        primaryGray1: '#E3E3E3',
+        primaryGray2: '#BBE5C9',
+      },
+      boxShadow: {
+        shadow1: '-9px -9px 16px 0px rgba(238, 238, 238, 0.60), 10px 10px 17px 0px rgba(193, 208, 195, 0.26)',
+        shadow1Hover: '-9px -9px 16px 0px rgba(255, 255, 255, 0.60), 12px 12px 20px 0px rgba(164, 198, 163, 0.50)',
+        shadow2: '10px 10px 17px 0px rgba(194, 209, 196, 0.15)',
+        shadow3: '0px 22px 30px 0px rgba(16, 112, 41, 0.08)',
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
