b6ce97219f61d2fccbdf41231db7f9224e90245c
Joyonto Karmakar <joyonto1997@gmail.com>
2023-07-19 00:45:56 +0600

    image render issue solve ongoing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import {Link} from "react-router-dom";
+import logo from '../assets/images/main-logo.png'
 
 export default function Header() {
     return (
         <div className={'px-[120px] py-6 flex justify-between border-0 border-[#F3F3F3] shadow-shadow1'}>
             <div className={'flex justify-start items-center gap-x-[150px]'}>
                 <Link to={'/'}>
-                    <img src={'/src/assets/images/main-logo.png'} alt="" className={'h-10 w-[202px]'}/>
+                    <img src={logo} alt="" className={'h-10 w-[202px]'}/>
                 </Link>
                 <div className={'flex justify-start items-center gap-x-20'}>
                     <select name="" id="" className={'text-xl font-medium text-primary'}>
