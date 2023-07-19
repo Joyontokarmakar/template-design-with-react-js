632a25f8e9bce38a6c12439f984ebb9b25840377
Joyonto Karmakar <joyonto1997@gmail.com>
2023-07-19 17:12:50 +0600

    data updated according to figma

--- src/components/mainSection/StatSection.jsx
+++ src/components/mainSection/StatSection.jsx
@@ -2,7 +2,7 @@
 function StatSection() {
     return (
         <div className={'mx-5 2xl:mx-0 mt-10 mt-[98px]'}>
-            <p className={'text-black1 text-[35px] 2xl:text-[51px] font-medium w-4/5 lg:w-1/2 mx-auto text-center'}>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit</p>
+            <p className={'text-black1 text-3xl 2xl:text-[51px] font-medium w-4/5 lg:w-1/2 mx-auto text-center'}>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit</p>
             <div className={'mt-5 lg:mt-0 container mx-auto bg-transparent shadow-shadow2 2xl:max-w-[1675px] overflow-x-auto'}>
                 <table className={'w-full bg-transparent rounded-md table-auto'}>
                     <thead className={'bg-primaryGray1 text-black'}>
@@ -31,59 +31,59 @@ function StatSection() {
                         <td className={'statTableData bg-greenShed2 font-normal'}>3.16</td>
                     </tr>
                     <tr>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>Youtube</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>Male_18+_UGC_Video</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$2,567.00</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>120</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>90</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$10,800.00</td>
-                        <td className={'statTableData bg-greenShed3 font-extrabold'}>4.21</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$28.52</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>3.16</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>Adwords</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>US_Generic_Search</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$1,233.00</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>98</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>71</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$5,880.00</td>
+                        <td className={'statTableData bg-greenShed3 font-extrabold'}>4.77</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$17.48</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>3.43</td>
                     </tr>
                     <tr>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>Youtube</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>Male_18+_UGC_Video</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$2,567.00</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>120</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>90</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$10,800.00</td>
-                        <td className={'statTableData bg-greenShed3 font-extrabold'}>4.21</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$28.52</td>
-                        <td className={'statTableData bg-greenShed3 font-normal'}>3.16</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>Adwords</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>SKU2343_shoppong</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$3,120.00</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>150</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>127</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$14,700.00</td>
+                        <td className={'statTableData bg-greenShed3 font-extrabold'}>4.71</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$24.55</td>
+                        <td className={'statTableData bg-greenShed3 font-normal'}>3.99</td>
                     </tr>
                     <tr>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>Youtube</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>Male_18+_UGC_Video</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$2,567.00</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>120</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>90</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$10,800.00</td>
-                        <td className={'statTableData bg-greenShed2 font-extrabold'}>4.21</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$28.52</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>3.16</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>Facebook</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>All_25+_Static_split_screen</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$5,400.00</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>145</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>111</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$11,600.00</td>
+                        <td className={'statTableData bg-greenShed2 font-extrabold'}>2.15</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$48.54</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>1.65</td>
                     </tr>
                     <tr>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>Youtube</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>Male_18+_UGC_Video</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$2,567.00</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>120</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>90</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$10,800.00</td>
-                        <td className={'statTableData bg-greenShed4 font-extrabold'}>4.21</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$28.52</td>
-                        <td className={'statTableData bg-greenShed4 font-normal'}>3.16</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>Instagram</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>W_25+_Static_split_screen</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$3,210.00</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>280</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>177</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$23,800.00</td>
+                        <td className={'statTableData bg-greenShed4 font-extrabold'}>7.41</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$18.17</td>
+                        <td className={'statTableData bg-greenShed4 font-normal'}>4.68</td>
                     </tr>
                     <tr>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>Youtube</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>Male_18+_UGC_Video</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$2,567.00</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>TikTok</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>UGS_video_2</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$3,100.00</td>
                         <td className={'statTableData bg-greenShed2 font-normal'}>120</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>90</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$10,800.00</td>
-                        <td className={'statTableData bg-greenShed2 font-extrabold'}>4.21</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$28.52</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>3.16</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>110</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$7,080.00</td>
+                        <td className={'statTableData bg-greenShed2 font-extrabold'}>2.28</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$28.14</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>2.10</td>
                     </tr>
                     </tbody>
                 </table>
--- src/components/mainSection/TableDataSection.jsx
+++ src/components/mainSection/TableDataSection.jsx
@@ -33,76 +33,76 @@ function TableDataSection() {
                         <td className={'statTableData bg-primaryGray1 font-normal'}>$309</td>
                     </tr>
                     <tr>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>06-17 - 06-23</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>1476</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$209</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$219</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$230</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$242</td>
-                        <td className={'statTableData bg-greenShed4 font-normal'}>$254</td>
-                        <td className={'statTableData bg-greenShed4 font-normal'}>$267</td>
-                        <td className={'statTableData bg-greenShed4 font-normal'}>$280</td>
-                        <td className={'statTableData bg-greenShed4 font-normal'}>$294</td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>06-24 - 06-30</td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>1678</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$212</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$223</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$234</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$245</td>
+                        <td className={'statTableData bg-greenShed4 font-normal'}>$258</td>
+                        <td className={'statTableData bg-greenShed4 font-normal'}>$271</td>
+                        <td className={'statTableData bg-greenShed4 font-normal'}>$284</td>
+                        <td className={'statTableData bg-greenShed4 font-normal'}>$298</td>
                         <td className={'statTableData bg-primaryGray1 font-normal'}></td>
                     </tr>
                     <tr>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>06-17 - 06-23</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>1476</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$209</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$219</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$230</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$242</td>
-                        <td className={'statTableData bg-greenShed3 font-normal'}>$254</td>
-                        <td className={'statTableData bg-greenShed3 font-normal'}>$267</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$280</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}></td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}></td>
-                    </tr>
-                    <tr>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>06-17 - 06-23</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>1476</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>$209</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>$219</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>$230</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>$242</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>$254</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>$267</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}></td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>06-31- 07-06</td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>1561</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$210</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$221</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$232</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$243</td>
+                        <td className={'statTableData bg-greenShed3 font-normal'}>$255</td>
+                        <td className={'statTableData bg-greenShed3 font-normal'}>$268</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$281</td>
                         <td className={'statTableData bg-primaryGray1 font-normal'}></td>
                         <td className={'statTableData bg-primaryGray1 font-normal'}></td>
                     </tr>
                     <tr>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>06-17 - 06-23</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>1476</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$209</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$219</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$230</td>
-                        <td className={'statTableData bg-greenShed2 font-normal'}>$242</td>
-                        <td className={'statTableData bg-greenShed4 font-normal'}>$254</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}></td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>07-07 - 07-14</td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>2678</td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>$205</td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>$215</td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>$226</td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>$237</td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>$249</td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>$262</td>
                         <td className={'statTableData bg-primaryGray1 font-normal'}></td>
                         <td className={'statTableData bg-primaryGray1 font-normal'}></td>
                         <td className={'statTableData bg-primaryGray1 font-normal'}></td>
                     </tr>
                     <tr>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>06-17 - 06-23</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}>1476</td>
-                        <td className={'statTableData bg-greenShed3 font-normal'}>$209</td>
-                        <td className={'statTableData bg-greenShed3 font-normal'}>$219</td>
-                        <td className={'statTableData bg-greenShed3 font-normal'}>$230</td>
-                        <td className={'statTableData bg-greenShed4 font-normal'}>$242</td>
-                        <td className={'statTableData bg-primaryGray1 font-normal'}></td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>07-15 - 07-21</td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>1121</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$211</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$222</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$233</td>
+                        <td className={'statTableData bg-greenShed2 font-normal'}>$244</td>
+                        <td className={'statTableData bg-greenShed4 font-normal'}>$256</td>
                         <td className={'statTableData bg-primaryGray1 font-normal'}></td>
                         <td className={'statTableData bg-primaryGray1 font-normal'}></td>
                         <td className={'statTableData bg-primaryGray1 font-normal'}></td>
                         <td className={'statTableData bg-primaryGray1 font-normal'}></td>
                     </tr>
                     <tr>
-                    <td className={'statTableData bg-primaryGray1 font-normal'}>06-17 - 06-23</td>
-                    <td className={'statTableData bg-primaryGray1 font-normal'}>1476</td>
-                    <td className={'statTableData bg-greenShed4 font-normal'}>$209</td>
-                    <td className={'statTableData bg-greenShed4 font-normal'}>$219</td>
-                    <td className={'statTableData bg-greenShed4 font-normal'}>$230</td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>07-22 - 07-28</td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}>2210</td>
+                        <td className={'statTableData bg-greenShed3 font-normal'}>$220</td>
+                        <td className={'statTableData bg-greenShed3 font-normal'}>$231</td>
+                        <td className={'statTableData bg-greenShed3 font-normal'}>$243</td>
+                        <td className={'statTableData bg-greenShed4 font-normal'}>$255</td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}></td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}></td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}></td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}></td>
+                        <td className={'statTableData bg-primaryGray1 font-normal'}></td>
+                    </tr>
+                    <tr>
+                    <td className={'statTableData bg-primaryGray1 font-normal'}>07-29 - 08-04</td>
+                    <td className={'statTableData bg-primaryGray1 font-normal'}>1627</td>
+                    <td className={'statTableData bg-greenShed4 font-normal'}>$225</td>
+                    <td className={'statTableData bg-greenShed4 font-normal'}>$236</td>
+                    <td className={'statTableData bg-greenShed4 font-normal'}>$248</td>
                     <td className={'statTableData bg-primaryGray1 font-normal'}></td>
                     <td className={'statTableData bg-primaryGray1 font-normal'}></td>
                     <td className={'statTableData bg-primaryGray1 font-normal'}></td>
